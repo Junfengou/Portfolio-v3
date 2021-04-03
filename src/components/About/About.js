652622a70d36fedaeaccb8fecab771a81dea92ae
Junfeng ou <junfengou@gmail.com>
2021-04-03 10:55:26 -0500

    minor update on bio

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,7 +9,7 @@ function About() {
             <div className="wrapper" ref={ref}>
                 <h1 className={`${inView ? "isVisible" : null}`}>Hi! My name is Jun.</h1>
                 <h3 className={`${inView ? "isVisible" : null}`}>I'm an web developer from Omaha, Nebraska who enjoy to create things that live on the internet.
-                    My passion in web development sparked in 2019 when a friend of mine show me his web project. 
+                    My passion in web development sparked in 2019 when I decided to make a career change, and I felt in love with the craft.
                     The power of combining art and code to create something truly unique was something I have never thought possible.
                     From that moment on, I have decided to embark on this journey of web discovery.
                 </h3>
