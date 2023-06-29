073426c87be3ff5f04995b58e49f67b8a0c2102c
Junfeng Ou <jun@Junfengs-MacBook-Pro.local>
2023-06-29 12:58:26 -0500

    small update on the about section for grammar error

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,10 +9,9 @@ function About() {
             <div className="wrapper" ref={ref}>
                 <h1 className={`${inView ? "isVisible" : null}`}>Hi! My name is Jun.</h1>
                 <h3 className={`${inView ? "isVisible" : null}`}>
-                    I'm an web developer who enjoy to create things that live on the internet.
-                    Over the past few years, I have learned and worked on bunch of different web
-                    technologies. I enjoy creating UI components, But I also believe that you can't have a 
-                    good application without a strong backend business logics!
+                I'm a web developer who enjoys creating things that live on the internet. 
+                Over the past few years, I have learned and worked on a variety of different web technologies. 
+                I enjoy creating UI components, but also believe you can't have a good application without strong backend business logic!
                 </h3>
             </div>
         </AboutStyles>
